f4a0e2e25282710ee7c688cb23e1430017c9652e
AINOI <90683803+AINOI@users.noreply.github.com>
2021-11-15 11:54:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -30,6 +30,6 @@ bot.on('message', async (event) => {
     }
   }
 })
-bot.listen('/', process.env.POST || 3000, () => {
+bot.listen('/', process.env.PORT || 3000, () => {
   console.log('機器人啟動。')
 })
